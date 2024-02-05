613c3d74159e4f1328deb4cb5472309064085963
kiwayu <henry.collister@googlemail.com>
2024-02-05 18:29:16 +0000

    Basic Weather Application Takes user location input and generates the conditions with an icon, gives humidity and windspeed

---
+++ src/AppLauncher.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                //display weather app gui
+                new WeatherAppGui().setVisible(true);
+
+                //System.out.println(WeatherApp.getLocationData("London"));
+
+                //System.out.println(WeatherApp.getCurrentTime());
+
+            }
+        });
+    }
+}
